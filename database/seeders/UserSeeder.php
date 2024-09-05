12c68f263b0c03cf514744b759b34e2ee585bdb3
rp.quraisy@gmail.com <rp.quraisy@gmail.com>
2024-09-05 11:02:15 +0700

    feat:add routes and some field

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -16,44 +16,44 @@ class UserSeeder extends Seeder
     {
         User::factory()->create([
             'name' => 'Super Admin',
-            // 'username' => 'superadmin',
-            // 'profile_image' => 'https://via.placeholder.com/150',
+            'username' => 'superadmin',
+            'profile_image' => 'https://via.placeholder.com/150',
             'email' => 'superadmin@hrm.com',
             'password' => Hash::make('12345678'),
-            // 'shift_id' => null,
-            // 'status' => 'Enable',
-            // 'department_id' => null,
-            // 'designation_id' => null,
-            // 'role_id' => \App\Models\Role::where('name', 'admin')->first()->id,
+            'shift_id' => null,
+            'status' => 'Enable',
+            'department_id' => null,
+            'designation_id' => null,
+            'role_id' => \App\Models\Role::where('name', 'admin')->first()->id,
 
-            // 'phone' => '085895226892',
-            // 'address' => 'Surabaya, East Java, Indonesia',
-            // 'company_id' => 1,
-            // 'is_superadmin' => 1,
-            // 'user_type' => 'employee',
-            // 'login_enabled' => 1,
+            'phone' => '085895226892',
+            'address' => 'Surabaya, East Java, Indonesia',
+            'company_id' => 1,
+            'is_superadmin' => 1,
+            'user_type' => 'employee',
+            'login_enabled' => 1,
             // 'created_by' => 1, // Assuming Super Admin is created by itself
         ]);
 
         User::factory()->create([
             'name' => 'Bahri',
-            // 'username' => 'bahri_admin',
-            // 'profile_image' => 'https://via.placeholder.com/150',
+            'username' => 'bahri_admin',
+            'profile_image' => 'https://via.placeholder.com/150',
             'email' => 'bahri@hrm.com',
             'password' => Hash::make('12345678'),
-            // 'shift_id' => null,
-            // 'status' => 'Enable',
-            // 'department_id' => null,
-            // 'designation_id' => null,
-            // 'role_id' => \App\Models\Role::where('name', 'admin')->first()->id,
+            'shift_id' => null,
+            'status' => 'Enable',
+            'department_id' => null,
+            'designation_id' => null,
+            'role_id' => \App\Models\Role::where('name', 'admin')->first()->id,
 
-            // 'phone' => '085895226892',
-            // 'address' => 'Surabaya, East Java, Indonesia',
-            // 'company_id' => 1,
-            // 'is_superadmin' => 0,
-            // 'user_type' => 'employee',
-            // 'login_enabled' => 1,
-            //'created_by' => 1, // Created by Super Admin
+            'phone' => '085895226892',
+            'address' => 'Surabaya, East Java, Indonesia',
+            'company_id' => 1,
+            'is_superadmin' => 0,
+            'user_type' => 'employee',
+            'login_enabled' => 1,
+            // 'created_by' => 1, // Created by Super Admin
         ]);
     }
 }
