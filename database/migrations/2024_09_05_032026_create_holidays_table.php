77e0a263c0207bd6ec52a20f04fed2d6953c5898
rp.quraisy@gmail.com <rp.quraisy@gmail.com>
2024-09-05 10:23:16 +0700

    feat:add CRUD functionality Holiday ApiController

---
+++ database/migrations/2024_09_05_032026_create_holidays_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('holidays', function (Blueprint $table) {
+            $table->id();
+            //             `company_id` bigint(20) unsigned DEFAULT NULL,
+            //   `name` varchar(191) NOT NULL,
+            //   `year` int(11) NOT NULL,
+            //   `month` int(11) NOT NULL,
+            //   `date` date NOT NULL,
+            //   `is_weekend` tinyint(1) NOT NULL DEFAULT 0,
+            //   `created_by` bigint(20) unsigned DEFAULT NULL,
+            $table->foreignId('company_id')->constrained('companies')->onDelete('cascade');
+            $table->string('name');
+            $table->integer('year');
+            $table->integer('month');
+            $table->date('date');
+            $table->boolean('is_weekend')->default(0);
+            $table->foreignId('created_by')->constrained('users')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('holidays');
+    }
+};
