b6eb9c57fd0b95243d11c5d190bd4220e5e9efdf
rp.quraisy@gmail.com <rp.quraisy@gmail.com>
2024-09-05 10:09:45 +0700

    feat:add CRUD functionality Shift on Api Controller

---
+++ database/migrations/2024_09_05_030719_create_shifts_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('shifts', function (Blueprint $table) {
+            $table->id();
+            // `company_id` bigint(20) unsigned DEFAULT NULL,
+            // `name` varchar(191) NOT NULL,
+            // `clock_in_time` time NOT NULL,
+            // `clock_out_time` time NOT NULL,
+            // `late_mark_after` int(11) DEFAULT NULL,
+            // `early_clock_in_time` int(11) DEFAULT NULL,
+            // `allow_clock_out_till` int(11) DEFAULT NULL,
+            $table->foreignId('company_id')->constrained('companies')->onDelete('cascade');
+            $table->string('name');
+            $table->time('clock_in_time');
+            $table->time('clock_out_time');
+            $table->integer('late_mark_after')->nullable();
+            $table->integer('early_clock_in_time')->nullable();
+            $table->integer('allow_clock_out_till')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('shifts');
+    }
+};
